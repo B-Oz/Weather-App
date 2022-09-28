837ba9fabe36014a71297969e66a59f1af85e568
B-Oz <bettysoftwareng@gmail.com>
2022-09-28 21:49:07 +0300

    js+

--- JS/weather.js
+++ JS/weather.js
@@ -5,10 +5,8 @@ const msg = document.querySelector("span.msg");
 const list = document.querySelector(".ajax-section .cities");
 
 localStorage.setItem(
-  "tokenKeyEncrypted",
-  EncryptStringAES(
-    "6/Cit6TZjKI4DKIhl/4Cf+Ai7Yz91X96Swt2Hp5kDTbGenKTFeRcFWyedjSJI3iH"
-  )
+  "tokenKey",
+  "6/Cit6TZjKI4DKIhl/4Cf+Ai7Yz91X96Swt2Hp5kDTbGenKTFeRcFWyedjSJI3iH"
 );
 
 form.addEventListener("submit", (event) => {
@@ -16,16 +14,45 @@ form.addEventListener("submit", (event) => {
   getWeatherDataFromApi();
 });
 
-//Get api function(http metthods ==verbs)
-
+//Get api func. (http methods == Verbs)
 const getWeatherDataFromApi = async () => {
   //alert("http request is gone!");
   const tokenKey = DecryptStringAES(localStorage.getItem("tokenKey"));
-  alert(tokenKey);
+
   const inputValue = input.value;
+  const units = "metric";
+  const lang = "tr";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${tokenKey}&units=${units}&lang=${lang}`;
 
   const response = await fetch(url).then((response) => response.json());
-
   console.log(response);
+  const { main, sys, weather, name } = response;
+
+  const iconUrl = `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
+
+  const iconUrlAWS = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0].icon}.svg`;
+
+  const cityNameSpans = list.querySelectorAll(".city span");
+  const cityNameSpansArray = Array.from(cityNameSpans);
+  if (cityNameSpansArray.length > 0) {
+  }
+
+  console.log(cityNameSpans);
+  const createdLi = document.createElement("li");
+  createdLi.classList.add("city");
+  createdLi.innerHTML = `<h2 class="city-name" data-name="${name}, ${
+    sys.country
+  }">
+  <span>${name}</span>
+  <sup>${sys.country}</sup>
+</h2>
+<div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+<figure>
+  <img class="city-icon" src="${iconUrl}">
+  <figcaption>${weather[0].description}</figcaption>
+</figure>`;
+  /* figure ve figcaption kullanımı seo açısından google da öne çıkarır*/
+
+  list.prepend(createdLi);
+  form.reset();
 };
